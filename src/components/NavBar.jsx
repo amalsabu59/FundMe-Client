d2905724c67dc9c992ee28ba589451d7d4769de5
amalsabu59 <amalsabu59@gmail.com>
2023-03-12 20:07:40 +0530

    create campaign page updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
   // const { connect, address } = useStateContext();
-  const address = null;
+  const address = "0327wbnc78";
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
